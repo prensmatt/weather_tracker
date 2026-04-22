31f33cd5949287b7696f87fa55bb4546a0a76d71
Maleyka Heybatova <6nisan2005@gmail.com>
2026-04-22 09:48:55 +0400

    query function, finishing the project

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import(
 	"encoding/json"
-	"io/util"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -19,7 +19,7 @@ type weatherData struct{
 
 
 func loadApiConfig(filename string)(apiConfigData, error){
-	bytes, err := io.util.ReadFile(filename)
+	bytes, err := ioutil.ReadFile(filename)
 	if err != nil{
 		return apiConfigData{},err
 	}
@@ -33,5 +33,40 @@ func loadApiConfig(filename string)(apiConfigData, error){
 	return c, nil
 }
 
+func hello(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("hello from go!\n"))
+}
+func query(city string) (weatherData,error){
+	apiConfig,err := loadApiConfig(".apiConfig")
+	if err != nil{
+		return weatherData{},err
+	}
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil{
+		return weatherData{},err
+	}
+	defer resp.Body.Close()
 
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil{
+		return weatherData{},err
+	}
+	return d, nil
+}
+
+func main(){
+	http.HandleFunc("/hello",hello)
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request){
+		city := strings.SplitN(r.URL.Path,"/",3)[2]
+		data,err := query(city)
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type","application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
 
